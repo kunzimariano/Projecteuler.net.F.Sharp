0e28a1d5944e04138df7b4ab3feacfa00dd85e04
kunzimariano <kunzi.mariano@gmail.com>
2014-03-19 13:20:01 -0300

    Fixing issue with every problem being calculated, even though their function wasn't being called.

--- ProjectEuler/Program.fs
+++ ProjectEuler/Program.fs
@@ -3,7 +3,6 @@
 module Program = 
     [<EntryPoint>]
     let main args = 
-        let r = Problems.Problem1.sumOfMultiples
-        printf "result is: %d" r
-        let input = System.Console.Read()
+        Problem1.printResult()
+        let input = System.Console.Read() 
         0
