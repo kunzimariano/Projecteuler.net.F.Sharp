da98f1bfc7b36c81652874f7acb7270612159a23
kunzimariano <kunzi.mariano@gmail.com>
2014-04-01 14:16:15 -0300

    Problem 6

--- Problems/Problems.fs
+++ Problems/Problems.fs
@@ -65,3 +65,18 @@ module Problem5 =
         
         let result = seq { 20..2..300000000 } |> Seq.find (fun x -> (isDivisible x))
         printf "result is: %d" result
+
+module Problem6 = 
+    //The sum of the squares of the first ten natural numbers is, 1^2 + 2^2 + ... + 10^2 = 385
+    //The square of the sum of the first ten natural numbers is, (1 + 2 + ... + 10)^2 = 55^2 = 3025
+    //Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+    //Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+    let printResult() = 
+        let sumOfSquares = 
+            [ 1.0..100.0 ]
+            |> Seq.map (fun x -> x * x)
+            |> Seq.fold (+) 0.0        
+        
+        let squareOfSums = ([ 1.0..100.0 ] |> Seq.fold (+) 0.0) ** 2.0
+
+        printf "result is: %A" ( squareOfSums - sumOfSquares)
