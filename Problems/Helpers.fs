9ead199801f8d147162f40080bf521076d35be26
kunzimariano <kunzi.mariano@gmail.com>
2014-04-01 17:07:55 -0300

    Problem 7

--- Problems/Helpers.fs
+++ Problems/Helpers.fs
@@ -2,9 +2,11 @@
 
 module Helpers = 
     let isPrime n = 
-        seq { 2L..int64 (System.Math.Sqrt(float n)) }
-        |> Seq.exists (fun x -> (n % x = 0L) || n % (x + 2L) = 0L)
-        |> not
+        if n < 2L then false
+        else 
+            seq { 2L..int64 (System.Math.Sqrt(float n)) }
+            |> Seq.exists (fun x -> (n % x = 0L) || n % (x + 2L) = 0L)
+            |> not
     
     let isFactor (candidate : int64) n = (n % candidate) = 0L
     let isEven n = (n % 2 = 0)
